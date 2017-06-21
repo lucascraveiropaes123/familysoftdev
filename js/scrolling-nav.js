a01c877968dba6c18885d8c84998c3fdb2023778
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-06-21 10:55:58 -0300

    Add Page Files

---
+++ js/scrolling-nav.js
@@ -0,0 +1,28 @@
+//jQuery to collapse the navbar on scroll
+/*$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});*/
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top < 80) {
+        $(".navbar-fixed-top").css("display", "none");
+    } else {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").css("display", "block");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
